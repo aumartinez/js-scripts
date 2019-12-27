776714e1b043ab3cef3054a0e4c4929bdd4e0321
Augusto Martinez <acm_asd@hotmail.com>
2019-12-27 09:55:53 -0600

    Update robot.js

--- robot.js
+++ robot.js
@@ -1,1 +1,12 @@
 //Delivery robot sample
+
+//Array group from town map image with points grouped by pair
+const points = [
+  "Alice's House-Bob's House",   "Alice's House-Cabin",
+  "Alice's House-Post Office",   "Bob's House-Town Hall",
+  "Daria's House-Ernie's House", "Daria's House-Town Hall",
+  "Ernie's House-Grete's House", "Grete's House-Farm",
+  "Grete's House-Shop",          "Marketplace-Farm",
+  "Marketplace-Post Office",     "Marketplace-Shop",
+  "Marketplace-Town Hall",       "Shop-Town Hall"
+];
